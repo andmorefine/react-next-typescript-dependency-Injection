dc6f746f6b447ce3703ef5b96785ff8ea7aab6a0
tanaka.satoshi <tanaka.satoshi@a-tm.co.jp>
2024-03-24 15:26:18 +0900

    create next

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# react-next-typescript-dependency-Injection
+# react-next-typescript-dependency-injection
+
+## React.js、Next.js、TypeScriptを例に、依存関係の反転と注入とは何か
+
+関連記事：[フロントエンドにおける依存性の注入とは何か]()
+
---
+++ src/app/login/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { FC, useState } from 'react'
+
+const LoginPage: FC = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = () => {
+    fetch('https://api.com/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Login failed')
+        }
+        const user = response.json()
+        console.log('Login successful! User:', user)
+      })
+      .catch((error) => {
+        console.error('Login error:', error)
+        throw error
+      })
+  }
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  )
+}
+
+export default LoginPage
